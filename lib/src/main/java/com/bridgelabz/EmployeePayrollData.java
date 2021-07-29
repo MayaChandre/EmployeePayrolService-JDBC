9b80b6b96bdf198e2a28aa020d621b7a7ad1006c
mayavathi <mayachandre27@gmail.com>
2021-07-29 05:28:44 -0400

    Reading and writing employee data

--- lib/src/main/java/com/bridgelabz/EmployeePayrollData.java
+++ lib/src/main/java/com/bridgelabz/EmployeePayrollData.java
@@ -6,14 +6,16 @@ public class EmployeePayrollData {
     public String name;
     public double salary;
 
-    public EmployeePayrollData(Integer id, String name, Double salary) {
+    public EmployeePayrollData(Integer id, String name, Double salary) 
+    {
         this.id = id;
         this.name = name;
         this.salary = salary;
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "EmployeePayrollData [id=" + id + ", name=" + name + ", salary=" + salary + "]";
     }
 
